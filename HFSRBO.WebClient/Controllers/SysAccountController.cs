5b3dcd58313fc46c7ccb6654cbd130f24961d9d8
= <=>
2019-03-07 17:40:48 +0800

    Push Update

--- HFSRBO.WebClient/Controllers/SysAccountController.cs
+++ HFSRBO.WebClient/Controllers/SysAccountController.cs
@@ -6,9 +6,13 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using HFSRBO.WebClient.Models;
+using System.Web.UI;
 
 namespace HFSRBO.WebClient.Controllers
 {
+    [Authorize]
+    [NoCache]
+    [OutputCache(Location = OutputCacheLocation.None, NoStore = true, Duration = 0)]
     public class SysAccountController : Controller
     {
         ApplicationDbContext context = new ApplicationDbContext();
@@ -24,6 +28,45 @@ namespace HFSRBO.WebClient.Controllers
         {
             var userlogins = context.Users.ToList();
             return View(userlogins);
+        }
+        [HttpGet]
+        public ActionResult CreateUser()
+        {
+            var roles = context.Roles.ToList();
+            return PartialView(roles);
+        }
+        [HttpPost]
+        public ActionResult CreateUser(FormCollection collection)
+        {
+            String username = collection.Get("username");
+            String password = collection.Get("password");
+            String role = collection.Get("role");
+
+            var user = new ApplicationUser();
+            user.UserName = username;
+            user.Email = username+"@gmail.com";
+
+            string userPWD = password;
+
+            var chkUser = UserManager.Create(user, userPWD);
+
+            //Add default User to Role Admin   
+            if (chkUser.Succeeded)
+            {
+                var result1 = UserManager.AddToRole(user.Id, role);
+            }
+
+            return RedirectToAction("UserAccounts");
+        }
+        public ActionResult DeleteUser(String ID)
+        {
+            var user = context.Users.Where(p => p.Id == ID).FirstOrDefault();
+            if (user.UserName != "bhfs_admin")
+            {
+                context.Users.Remove(user);
+                context.SaveChanges();
+            }
+            return RedirectToAction("UserAccounts");
         }
     }
 }
